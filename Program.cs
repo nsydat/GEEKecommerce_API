b9a79b81c163861b87a5347e6194c31c051f49ae
Nong Sy Dat <144932990+nsydat@users.noreply.github.com>
2024-12-18 21:26:23 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,44 +17,32 @@ builder.Services.AddSwaggerGen(c =>
 
 builder.Services.AddControllers();
 
-// Cấu hình DbContext với SQL Server
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), sqlOptions =>
-        sqlOptions.CommandTimeout(180) // Timeout cho câu lệnh SQL
+        sqlOptions.CommandTimeout(180) 
     ));
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "GEEK-Commerce API V1");
-    });
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error"); // Xử lý lỗi
-    app.UseHsts(); // Sử dụng HSTS để tăng cường bảo mật
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "GEEK-Commerce API V1");
+    options.RoutePrefix = String.Empty;
+});
 
-// Cấu hình CORS
+
 app.UseCors("AllowAll");
 
-// Middleware mặc định
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-// Routing và Authorization
 app.UseRouting();
 app.UseAuthorization();
 
-// Cấu hình endpoint cho Controllers
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-// Chạy ứng dụng
 app.Run();
 
